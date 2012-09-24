58a24a8a663f3571295fe2e9500116f17b8bb5cd
Kristian Bjørnard <kristian.bjornard@gmail.com>
2012-09-24 15:35:24 -0400

    does this fix the fact that my custom js won'r load?

--- js/script.js
+++ js/script.js
@@ -6,16 +6,10 @@
  * the README.txt next to this file.
  */
 
-// JavaScript should be made compatible with libraries other than jQuery by
-// wrapping it with an "anonymous closure". See:
-// - http://drupal.org/node/1446420
-// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
-(function ($, Drupal, window, document, undefined) {
 
 
-// Place your code here.
 
-    jQuery('#block-views-frontpage-slideshow-block-1 .view-display-id-block_1 .view-content')
+    $('#block-views-frontpage-slideshow-block-1 .view-display-id-block_1 .view-content')
       .after('<div id="slidenav">')
       .cycle({ 
         fx:     'fade', 
@@ -24,5 +18,18 @@
         pager:  '#slidenav'
     });
 
+
+
+
+
+
+
+// JavaScript should be made compatible with libraries other than jQuery by
+// wrapping it with an "anonymous closure". See:
+// - http://drupal.org/node/1446420
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+(function ($, Drupal, window, document, undefined) {
+
+// Place your code here.
 
 })(jQuery, Drupal, this, this.document);
